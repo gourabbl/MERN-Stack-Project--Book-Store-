6041419dfb040c787f98bea78cc2cb4a60b7358e
gourabbl <gourabbal6@gmail.com>
2024-03-14 04:06:28 +0530

    MERN Stack Project
    Learnt Backend CRUD, Backend Router, CORS Policy, MongoDB operations, Frontend CRUD, Frontend Router

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:<password>@book-store-mern.m9oqlex.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
